bd3be656b4b0951283640d62593144219f1d2c10
Alex Tseniklidou <atseniklidou@live.com>
2018-12-01 16:08:25 +0200

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+// Include the namespace required to use Unity UI
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	// Create public variables for player speed, and for the Text UI game objects
+	public float speed;
+
+	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
+	private Rigidbody rb;
+    private Renderer rend;
+
+	// At the start of the game..
+	void Start ()
+	{
+		// Assign the Rigidbody component to our private rb variable
+		rb = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+	}
+
+	// Each physics step..
+	void FixedUpdate ()
+	{
+		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		// Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
+		// multiplying it by 'speed' - our public player speed that appears in the inspector
+		rb.AddForce (movement * speed);
+	}
+
+	// When this game object intersects a collider
+    void OnCollisionEnter(Collision collision) 
+	{
+        // Set the player's Color to that od the collided object
+        Color hitColor = collision.transform.GetComponent<Renderer>().material.color;
+        rend.material.color = hitColor;
+
+        //Find the Specular shader and change its Color to red
+     //   rend.material.shader = Shader.Find("Specular");
+     //   rend.material.SetColor("_SpecColor", Color.red);
+    }
+}
